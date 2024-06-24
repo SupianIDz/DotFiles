15efa91d468d86e19504d400ab2d00ce68e313f2
SupianIDz <supianidz@gmail.com>
2024-06-25 01:56:55 +0800

    jenv Init

--- loader.zsh
+++ loader.zsh
@@ -5,3 +5,5 @@ done
 
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+eval "$(jenv init -)"
