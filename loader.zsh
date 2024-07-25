a1d1a3349885f3ac80ff63440d54ba460a75cd7a
Supian M <supianidz@gmail.com>
2024-07-25 14:24:12 +0800

    WIP

--- loader.zsh
+++ loader.zsh
@@ -1,9 +1,16 @@
+isMacOS() {
+  [[ "$(uname)" == "Darwin" ]]
+}
+
 for file in ~/.dotfiles/shell/{variable,xaliases,function}.zsh; do
 	# shellcheck source=./${file}
 	[ -r "$file" ] && [ -f "$file" ] && source "${file}"
 done
 
-source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if isMacOS; then
 
-eval "$(jenv init -)"
+  source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+  source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+  eval "$(jenv init -)"
+fi
--- shell/function.zsh
+++ shell/function.zsh
@@ -1,80 +1,85 @@
 function db() {
-    if [ "$1" = "create" ]; then
-        if mysql -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "DROP DATABASE IF EXISTS $2; CREATE DATABASE $2"; then
-            echo "DATABASE $2 SUCCESSFULLY CREATED."
-        else
-            echo "FAILED TO CREATE DATABASE $2."
-        fi
-    elif [ "$1" = "drop" ]; then
-        if mysql -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "DROP DATABASE $2"; then
-            echo "DATABASE $2 SUCCESSFULLY DROPPED."
-        else
-            echo "FAILED TO DROP DATABASE $2."
-        fi
-    elif [ "$1" = "list" ]; then
-        if mysql -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASES"; then
-            echo "DATABASES LISTED SUCCESSFULLY."
-        else
-            echo "FAILED TO LIST DATABASES."
-        fi
-    elif [ "$1" = "import" ]; then
-        if mysql -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" $2 <$3; then
-            echo "DATABASE $2 SUCCESSFULLY IMPORTED FROM $3."
-        else
-            echo "FAILED TO IMPORT DATABASE $2 FROM $3."
-        fi
-    elif [ "$1" = "export" ]; then
-        if mysqldump -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" $2 >$3; then
-            echo "DATABASE $2 SUCCESSFULLY EXPORTED TO $3."
-        else
-            echo "FAILED TO EXPORT DATABASE $2 TO $3."
-        fi
+  if [ "$1" = "create" ]; then
+    if mysql -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "DROP DATABASE IF EXISTS $2; CREATE DATABASE $2"; then
+      echo "DATABASE $2 SUCCESSFULLY CREATED."
     else
-        echo "No arguments provided. Please provide the necessary arguments."
-        echo "Usage:"
-        echo "create <database>: to create a new database."
-        echo "drop <database>: to drop an existing database."
-        echo "list: to list all databases."
-        echo "import <database> <file>: to import a database from a file."
-        echo "export <database> <file>: to export a database to a file."
+      echo "FAILED TO CREATE DATABASE $2."
     fi
+  elif [ "$1" = "drop" ]; then
+    if mysql -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "DROP DATABASE $2"; then
+      echo "DATABASE $2 SUCCESSFULLY DROPPED."
+    else
+      echo "FAILED TO DROP DATABASE $2."
+    fi
+  elif [ "$1" = "list" ]; then
+    if mysql -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASES"; then
+      echo "DATABASES LISTED SUCCESSFULLY."
+    else
+      echo "FAILED TO LIST DATABASES."
+    fi
+  elif [ "$1" = "import" ]; then
+    if mysql -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" $2 <$3; then
+      echo "DATABASE $2 SUCCESSFULLY IMPORTED FROM $3."
+    else
+      echo "FAILED TO IMPORT DATABASE $2 FROM $3."
+    fi
+  elif [ "$1" = "export" ]; then
+    if mysqldump -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" $2 >$3; then
+      echo "DATABASE $2 SUCCESSFULLY EXPORTED TO $3."
+    else
+      echo "FAILED TO EXPORT DATABASE $2 TO $3."
+    fi
+  else
+    echo "No arguments provided. Please provide the necessary arguments."
+    echo "Usage:"
+    echo "create <database>: to create a new database."
+    echo "drop <database>: to drop an existing database."
+    echo "list: to list all databases."
+    echo "import <database> <file>: to import a database from a file."
+    echo "export <database> <file>: to export a database to a file."
+  fi
 }
 
 function phpv() {
+  if isMacOS; then
+
     local desired=$1
     local available=(
-        "8.4"
-        "8.3"
-        "8.2"
-        "8.1"
-        "8.0"
-        "7.4"
-        "7.3"
-        "7.2"
-        "7.1"
-        "7.0"
-        "5.6"
+      "8.4"
+      "8.3"
+      "8.2"
+      "8.1"
+      "8.0"
+      "7.4"
+      "7.3"
+      "7.2"
+      "7.1"
+      "7.0"
+      "5.6"
     )
 
     # Check if the desired PHP version is available
     if [[ " ${available[*]} " == *" $desired "* ]]; then
-        # Unlink all versions of PHP
-        for version in "${available[@]}"; do
-            if [[ "$version" != "$desired" ]]; then
-                brew unlink "php@$version" 2>/dev/null
-            fi
-        done
+      # Unlink all versions of PHP
+      for version in "${available[@]}"; do
+        if [[ "$version" != "$desired" ]]; then
+          brew unlink "php@$version" 2>/dev/null
+        fi
+      done
 
-        # Link the desired PHP version
-        brew link "php@$desired" --force --overwrite &>/dev/null
+      # Link the desired PHP version
+      brew link "php@$desired" --force --overwrite &>/dev/null
 
-        # Check the PHP version
-        php -v
+      # Check the PHP version
+      php -v
     else
-        echo "PHP version $desired is not available."
+      echo "PHP version $desired is not available."
     fi
+  else
+    echo "Asu"
+  fi
 }
 
 function unrar() {
-    find . -name '*.rar' -execdir unar -s -p $1 {} \;
+  find . -name '*.rar' -execdir unar -s -p $1 {} \;
 }
